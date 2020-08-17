a532efff8cae3e51373b03889631b1d8b303b02d
khalid almuhawis <khalidalmuhawis0@gmail.com>
2020-08-17 08:47:36 +0300

    madlibs game

--- madlibs.py
+++ madlibs.py
@@ -1,5 +1,13 @@
 def main():
-	# write your code here
+	time = input("Enter a number from 1 to 12: ")
+	items = input("Enter a noun (plural): ")
+	name = input("Enter a name: ")
+	scream = input("Enter any sentence: ")
+	action = input("Enter a verb: ")
+
+	print("It was" + time + " o'clock when I heard a knock at the door. I opened the door and there was a box full of "
+	 + items + " with a note saying 'From Mr. " + name.title() +".' Just as I closed the door I heard a scream '"
+	 + scream.upper() + ".' I froze in place and all I could do was " + action + ".")
 
 
 
